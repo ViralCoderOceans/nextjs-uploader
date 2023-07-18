7641d8b0f641ca137904ebb11f6790963d7bcfec
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-18 19:32:50 +0530

    Add: constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,22 +1,28 @@
 export const TABS = [
   {
-    id:1,
-    name:'Facebook-page upload',
-    path:'to-facebook-page'
+    id: 1,
+    name: 'Facebook-page',
+    path: 'to-facebook-page'
   },
   {
-    id:2,
-    name:'Facebook-account upload',
-    path:'to-facebook-account'
+    id: 2,
+    name: 'Facebook-account',
+    path: 'to-facebook-account'
   },
   {
-    id:3,
-    name:'Instagram upload',
-    path:'to-instagram'
+    id: 3,
+    name: 'Instagram',
+    path: 'to-instagram'
   },
   {
-    id:4,
-    name:'Youtube upload',
-    path:'to-youtube'
+    id: 4,
+    name: 'Youtube',
+    path: 'to-youtube'
   },
 ]
+
+export const BASE_URL = 'https://graph.facebook.com/v17.0/'
+
+export const PAGE_ID = '106338012535000'
+
+export const SHORT_TIME_FB_AT = 'EAADAbiWA4R8BAB6JM6ZCfu1FRpPRhHXkqNdfDJKVF02jov3svIosgdi5yuEC1W7dQQZAKJlztgmGnmOGyyQ0KNNKRnfvTQUMtohYjx0ZASSMTKsrDDFzGLhxO7hX2XZAhxyFqetgJz6IYNZCqEKHqO2XITP1ZAIIbZCiQPKEMQqYxsQvcAoy3QPzR6qwshQwvmSLhT6GZC9FVoY9bZBU07KbD'
