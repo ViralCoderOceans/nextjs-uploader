9ed6fd435bc1427a15db928afd9cf01d13da2f01
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-20 18:46:34 +0530

    update: store fb-login into local-storage

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import SideBar from '@/components/SideBar'
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +14,15 @@ export default function RootLayout({ children }) {
   const [fbLoginData, setFbLoginData] = useState()
   const [isFBLogin, setIsFBLogin] = useState(false)
   const [isFBPosting, setIsFBPosting] = useState(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('isFBLogin')) {
+      setIsFBLogin(localStorage.getItem('isFBLogin'))
+    } else {
+      localStorage.setItem('isFBLogin', isFBLogin)
+    }
+  }, [])
+
   return (
     <html lang="en">
       <head>
